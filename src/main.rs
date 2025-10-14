49405f753d2dc86d4e8d410d2e22a9316719485a
Qai Juang <qaijuang@gmail.com>
2025-10-14 11:27:44 -0400

    feat: git sao

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg(unix)]
+
+use std::{
+    io,
+    process::{Command, ExitCode, Output, Stdio, Termination},
+};
+
+fn main() -> io::Result<OutputExt> {
+    let script = r#"
+        current=$(git branch --show-current) &&
+        default=$(git symbolic-ref --short refs/remotes/origin/HEAD) &&
+        default=${default#origin/} &&
+        git checkout "$default" &&
+        git pull --ff-only origin &&
+        git branch -D "$current" &&
+        git push origin --delete "$current"
+    "#;
+
+    Command::new("sh")
+        .args(["-c", script])
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .map(OutputExt)
+}
+
+// std::process::Output lacks Termination impl
+struct OutputExt(Output);
+
+impl Termination for OutputExt {
+    fn report(self) -> ExitCode {
+        if self.0.status.success() {
+            ExitCode::SUCCESS
+        } else {
+            ExitCode::FAILURE
+        }
+    }
 }
