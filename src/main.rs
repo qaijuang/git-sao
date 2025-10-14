02e70f40fdf80d8bb4cb2e789cab5065e4dd14cc
Success Kingsley <successxodev@gmail.com>
2025-10-14 14:07:36 -0400

    feat: git sao windows

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-#![cfg(unix)]
-
 use std::{
     io,
     process::{Command, ExitCode, Output, Stdio, Termination},
 };
 
 fn main() -> io::Result<OutputExt> {
-    let script = r#"
+    #[cfg(unix)]
+    {
+        let script = r#"
         current=$(git branch --show-current) &&
         default=$(git symbolic-ref --short refs/remotes/origin/HEAD) &&
         default=${default#origin/} &&
@@ -16,12 +16,47 @@ fn main() -> io::Result<OutputExt> {
         git push origin --delete "$current"
     "#;
 
-    Command::new("sh")
-        .args(["-c", script])
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()
-        .map(OutputExt)
+        Command::new("sh")
+            .args(["-c", script])
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output()
+            .map(OutputExt)
+    }
+
+    #[cfg(windows)]
+    {
+        let script = r#"
+            $current = (git branch --show-current).Trim()
+            if (-not $current) { exit $LASTEXITCODE }
+
+            $default = (git symbolic-ref --short refs/remotes/origin/HEAD).Trim()
+            if (-not $default) { exit $LASTEXITCODE }
+            $default = $default -replace '^origin/', ''
+
+            function Run($cmd) {
+                & $cmd
+                if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+            }
+            Run { git checkout $default }
+            Run { git pull --ff-only origin }
+            Run { git branch -D $current }
+            Run { git push origin --delete $current }
+        "#;
+
+        Command::new("powershell")
+            .args([
+                "-NoProfile",
+                "-ExecutionPolicy",
+                "Bypass",
+                "-Command",
+                script,
+            ])
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output()
+            .map(OutputExt)
+    }
 }
 
 // std::process::Output lacks Termination impl
